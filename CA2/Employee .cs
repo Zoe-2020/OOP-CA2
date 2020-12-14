91b1aabedf34652317f4864198d314274c2de0cb
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2020-12-14 21:14:12 +0000

    Q10

--- CA2/Employee .cs
+++ CA2/Employee .cs
@@ -6,10 +6,106 @@ using System.Threading.Tasks;
 
 namespace CA2
 {
-    class Employee
+    abstract class Employee
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public decimal CalculateMonthlyPay { get; set; }
+
+        public string Shift {get; set;}
+
+       
+            
+
+        
+
+
+
+        // public abstract decimal CalculateMonthlyPay();
+
+        /*   public Employee(string firstName, string lastName)
+           {
+               FirstName = firstName;
+               LastName = lastName;
+
+           } */
+        /* public override string ToString()
+         {
+             return string.Format($"{LastName.ToUpper()},{FirstName}");
+         } */
+    }
+     class FullTimeEmployee : Employee
+    {
+
+        public decimal Salary { get; set; }
+
+        public FullTimeEmployee (string firstName, string lastName,string shift ,decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Shift = shift;
+            Salary = salary;
+        } 
+
+        public override string ToString()
+        {
+            return string.Format($"{LastName.ToUpper()},{FirstName}-{Shift}");
+        }
+        /*  public decimal Salary { get; set; }
+
+        //  public string Shift { get; set; }
+       //   public int NumHoildaysDays { get; set; }
+
+
+          public  FullTimeEmployee (string firstName, string lastName,decimal salary)
+          {
+              salary = Salary;
+
+          }
+
+          CalculateMonthlyPay()
+          {
+              return Salary / 12;
+          } */
+
+    }
+
+    class PartTimeEmployee : Employee
+    {
+
+        public decimal Salary { get; set; }
+        public decimal HourlyRate { get; set; }
+        public double HoursWorked { get; set; }
+
+
+        public PartTimeEmployee(string firstName, string lastName,string shift, decimal hourlyRate , double hoursWorked)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Shift = shift;
+            HourlyRate = hourlyRate;
+            HoursWorked = hoursWorked;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{LastName.ToUpper()},{FirstName}-{Shift}");
+        }
+        /*  public decimal Salary { get; set; }
+
+        //  public string Shift { get; set; }
+       //   public int NumHoildaysDays { get; set; }
+
+
+          public  FullTimeEmployee (string firstName, string lastName,decimal salary)
+          {
+              salary = Salary;
+
+          }
+
+          CalculateMonthlyPay()
+          {
+              return Salary / 12;
+          } */
+
     }
 }
--- CA2/MainWindow.xaml.cs
+++ CA2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,16 +27,113 @@ namespace CA2
         {
             InitializeComponent();
         }
-        
+
+        List <Employee> employees = new List <Employee>();
+        List<FullTimeEmployee> fullTimeEmployees = new List<FullTimeEmployee>();
+        List<PartTimeEmployee> partTimeEmployees = new List<PartTimeEmployee>();
+
+
+
+        private void EmployeeLB_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+            // string[] LastName = { "Jones", "Murphy", "Smith", "Walsh" };
+            /* Employee e1 = new Employee("Jane", "Jones");
+             Employee e2 = new Employee("Joe", "Murphy");
+             Employee e3 = new Employee("John", "Smith");
+             Employee e4 = new Employee("Jess", "Walsh"); */
+
+            PartTimeEmployee e1 = new PartTimeEmployee("Jane", "Jones", "Part Time", 5, 88);
+            FullTimeEmployee e2 = new FullTimeEmployee("Joe", "Murphy","Full Time", 5000);
+            PartTimeEmployee e3 = new PartTimeEmployee("John", "Smith", "Part Time",8,9);
+            FullTimeEmployee e4 = new FullTimeEmployee("Joe", "Murphy", "Full Time", 99050);
+
+            employees.Add(e1);
+            employees.Add(e2);
+            employees.Add(e3);
+            employees.Add(e4);
+
+            partTimeEmployees.Add(e1);
+            partTimeEmployees.Add(e3);
+            fullTimeEmployees.Add(e2);
+            fullTimeEmployees.Add(e4);
+
+
+
+
+
+            employees.OrderBy(x => x.LastName).ThenBy(x => x.FirstName);
+
+            EmployeeLB.ItemsSource = employees;
+      
+
             
 
+        }
 
-            private void EmployeeLB_Loaded(object sender, RoutedEventArgs e)
+        private void FirstNameTBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            FirstNameTBox.Clear();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+          /*  string NewFirstName = FirstNameTBox.Text;
+            string NewLastName = SurnameTBox.Text;
+
+            Employee employee = new Employee(NewFirstName , NewLastName );
+
+         
+            employees.Add(employee);
+
+            EmployeeLB.ItemsSource = null;
+            EmployeeLB.ItemsSource = employees;
+
+            //   EmployeeLB.ItemsSource = employees;
+
+            //  employees.Add(NewFirstName);
+            */
+
+
+        }
+
+        private void EmployeeLB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Employee selecetedEmployee = EmployeeLB.SelectedItem as Employee;
+            PartTimeEmployee selecetedPartTime= EmployeeLB.SelectedItem as PartTimeEmployee;
+            FullTimeEmployee selecetedFullTime = EmployeeLB.SelectedItem as FullTimeEmployee;
+
+            if (selecetedEmployee != null)
             {
-                string[] FirstName = { "Jane", "Joe", "John", "Jess" };
-                string[] LastName = { "Jones", "Murphy", "Smith", "Walsh" };
-                EmployeeLB.ItemsSource = FirstName;
+                FirstNameTBox.Text = selecetedEmployee.FirstName;
+                SurnameTBox.Text = selecetedEmployee.LastName;
 
+                if (selecetedEmployee.Shift == "Full Time")
+                {
+                    HoursWorkedTBox.Clear();
+                    HourlyRateTBox.Clear();
+
+                    SalaryTBox.Text = selecetedFullTime.Salary.ToString();
+
+                }
+                else
+                {
+                    SalaryTBox.Clear();
+                    HoursWorkedTBox.Text = selecetedPartTime.HourlyRate.ToString();
+                    HourlyRateTBox.Text = selecetedPartTime.HoursWorked.ToString();
+                }
+                
+                
+                
+
+            }
+
+        }
+
+        private void SurnameTBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            SurnameTBox.Clear();
         }
     }
 }
