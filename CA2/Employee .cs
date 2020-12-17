b57d77998e57ff9721ee229705c71d5fa11b0c87
Zoe-2020 <60262018+Zoe-2020@users.noreply.github.com>
2020-12-17 19:06:43 +0000

    added comments in class

--- CA2/Employee .cs
+++ CA2/Employee .cs
@@ -7,27 +7,30 @@ using System.Threading.Tasks;
 namespace CA2
 {
     abstract class Employee
-    {
+    {//take in first name , lastname and if employee is part time and last time 
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
         public string Shift {get; set;}
 
-
-         public abstract decimal CalculateMonthlyPay();
+        // and abstract method CalculateMonthlyPay
+        public abstract decimal CalculateMonthlyPay();
 
       
     }
+    //takes from employee class
      class FullTimeEmployee : Employee
     {
-
+       //full time needs salary
         public decimal Salary { get; set; }
 
+        //salary divided by 12  Calculate Monthly Pay
         public override decimal CalculateMonthlyPay()
         {
             return Salary / 12;
         } 
 
+        //fulltime employee have first name,surname shift and salary
         public FullTimeEmployee (string firstName, string lastName,string shift ,decimal salary)
         {
             FirstName = firstName;
@@ -36,26 +39,28 @@ namespace CA2
             Salary = salary;
         } 
 
+        //will display last name first in Uppercase , firstname and shift in listbox
         public override string ToString()
         {
             return string.Format($"{LastName.ToUpper()},{FirstName}-{Shift}");
         }
 
     }
-
+    //takes from employee class
     class PartTimeEmployee : Employee
     {
-
+        //partime employee need salary , hourly rate , hourworked 
         public decimal Salary { get; set; }
         public decimal HourlyRate { get; set; }
         public decimal HoursWorked { get; set; }
 
+        //hours rate by hours worked Calculate Monthly Pay
         public override decimal CalculateMonthlyPay()
         {
             return HourlyRate * HoursWorked;
         }
 
-
+        //parttime employee first name, surname,shift,hourly rate,hourly worked
         public PartTimeEmployee(string firstName, string lastName,string shift, decimal hourlyRate , decimal hoursWorked)
         {
             FirstName = firstName;
@@ -65,6 +70,7 @@ namespace CA2
             HoursWorked = hoursWorked;
         }
 
+        //display lastname , first and shift in listbox 
         public override string ToString()
         {
             return string.Format($"{LastName.ToUpper()},{FirstName}-{Shift}");
